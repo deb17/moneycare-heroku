9caeac35638bbf36cfaa419f4e9ea998cd9614fb
Debashish Palit <dpalit07@gmail.com>
2021-04-27 10:37:04 +0530

    Do auth on heroku

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, redirect, request, abort
+from flask import Flask, redirect, request, abort, url_for
+from flask_dance.contrib.google import make_google_blueprint, google
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '5ad1b20f9e1a66492a68e490e5abfb7d'
+app.config['GOOGLE_OAUTH_CLIENT_ID'] = '737155067926-jpco79782ombb1c068ifj9gln8ea7has.apps.googleusercontent.com'
+app.config['GOOGLE_OAUTH_CLIENT_SECRET'] = 'YJv1OLtf9sSkHntlHiHBcwCZ'
+app.config['OAUTHLIB_RELAX_TOKEN_SCOPE'] = True
+
+google_blueprint = make_google_blueprint(
+    scope=[
+        'openid',
+        'https://www.googleapis.com/auth/userinfo.email',
+        'https://www.googleapis.com/auth/userinfo.profile'],
+    redirect_to='google_login'
+)
 
 
 @app.route('/')
@@ -13,5 +28,21 @@ def index():
 
     if url == request.url:
         abort(400)
+
+    return redirect(url)
+
+
+@app.route('/google-login')
+def google_login():
+    if not google.authorized:
+        return redirect(url_for('google.login'))
+
+    resp = google.get('/oauth2/v1/userinfo')
+    retval = resp.json()
+
+    gname = retval['name']
+    gmail = retval['email']
+
+    url = f'https://moneycare.pythonanywhere.com/auth/google-login?name={gname}&email={gmail}'
 
     return redirect(url)
